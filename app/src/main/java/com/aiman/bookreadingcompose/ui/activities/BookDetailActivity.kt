577fa9c31a168326546d5e5f6a9cc7504f8c58dc
aimanmuzaffar95 <aimanmuzaffar95@gmail.com>
2021-06-07 12:30:13 +0500

    Changes: - Completed the design layout for Book Details Activity

--- app/src/main/java/com/aiman/bookreadingcompose/ui/activities/BookDetailActivity.kt
+++ app/src/main/java/com/aiman/bookreadingcompose/ui/activities/BookDetailActivity.kt
@@ -2,12 +2,14 @@ package com.aiman.bookreadingcompose.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Space
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.appcompat.widget.ButtonBarLayout
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -15,14 +17,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.Light
+import androidx.compose.ui.text.font.FontWeight.Companion.Normal
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.aiman.bookreadingcompose.R
+import com.aiman.bookreadingcompose.theme.CustomFont
 import com.aiman.bookreadingcompose.theme.CustomFont.robotoCondenseFamily
 
 class BookDetailActivity : AppCompatActivity() {
@@ -30,14 +41,24 @@ class BookDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        changeStatusBarColor(R.color.book_detail_background_color)
+
         setContent {
             MaterialTheme {
                 Column(modifier = Modifier
-                    .fillMaxSize()) {
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())) {
                     TopSection()
+                    BottomSection()
                 }
             }
         }
+    }
+
+    private fun changeStatusBarColor(color: Int) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, color)
     }
 
     @Composable
@@ -114,6 +135,8 @@ class BookDetailActivity : AppCompatActivity() {
             Spacer(modifier = Modifier.height(20.dp))
 
             DetailGrid()
+            Spacer(modifier = Modifier.height(32.dp))
+            ActionButtons()
 
         }
     }
@@ -186,6 +209,96 @@ class BookDetailActivity : AppCompatActivity() {
                     fontSize = 18.sp,
                     fontFamily = robotoCondenseFamily)
             }
+        }
+    }
+
+    @Composable
+    fun ActionButtons() {
+        Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+
+            ActionButton(text = getString(R.string.read_book), modifier =
+                Modifier.background(
+                        shape = RoundedCornerShape(topStartPercent = 20, bottomStartPercent = 20),
+                        color = colorResource(id = R.color.action_button_background_color)
+                    ),
+                R.drawable.ic_read_book
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            ActionButton(text = getString(R.string.listen_book), modifier =
+                Modifier.background(
+                        shape = RoundedCornerShape(topEndPercent = 20, bottomEndPercent = 20),
+                        color = colorResource(id = R.color.action_button_background_color)
+                    ),
+                R.drawable.ic_listen_audio
+            )
+        }
+    }
+
+    @Composable
+    fun ActionButton(text: String, modifier: Modifier, icon: Int) {
+        val buttonHeight = 50.dp
+        val buttonWidth = 170.dp
+
+        Row(
+            modifier = modifier
+                .height(buttonHeight)
+                .width(buttonWidth),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.width(2.dp))
+            Text(
+                text = text,
+                color = Color.White
+            )
+        }
+    }
+
+    @Composable
+    fun BottomSection() {
+        val headingFontSize = 22.sp
+        val contentFontSize = 18.sp
+
+        Column(modifier = Modifier.padding(12.dp)) {
+            Text(
+                "What is it about?",
+                color = Color.Black,
+                fontSize = headingFontSize,
+                fontFamily = robotoCondenseFamily,
+                fontWeight = SemiBold,
+            )
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus neque et libero tempus efficitur. Cras quam leo, egestas eu elit sit amet, elementum ultricies.",
+                color = Color.DarkGray,
+                fontSize = contentFontSize,
+                fontFamily = robotoCondenseFamily,
+                fontWeight = Normal,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                "Who is it for?",
+                color = Color.Black,
+                fontSize = headingFontSize,
+                fontFamily = robotoCondenseFamily,
+                fontWeight = SemiBold,
+            )
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus neque et libero tempus efficitur. Cras quam leo, egestas eu elit sit amet, elementum ultricies.",
+                color = Color.DarkGray,
+                fontSize = contentFontSize,
+                fontFamily = robotoCondenseFamily,
+                fontWeight = Normal,
+            )
         }
     }
 }
